5381128832d0f74b0d7308870426191d5fc4b0cd
levintoo <tooklevin@gmail.com>
2021-07-09 22:07:11 +0300

    fill rect

--- canvas.js
+++ canvas.js
@@ -5,14 +5,20 @@ window.addEventListener('load', () =>{
 
     // canvas resizie
     resize();
-
+  
+    ctx.strokeStyle = "blue";
+    ctx.strokeRect(50,50,200,200);
+    ctx.strokeStyle = "red";
+    ctx.strokeRect(100,100,200,200);
 });
-
 window.addEventListener('resize', resize)
-
-
 
 function resize(){
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
 }
+
+
+
+
+
