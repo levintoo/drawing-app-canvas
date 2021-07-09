b3ba03c5dba1910e6454434f962d090ebcbf0a5c
levintoo <tooklevin@gmail.com>
2021-07-09 22:12:00 +0300

    moving lines

--- canvas.js
+++ canvas.js
@@ -6,10 +6,18 @@ window.addEventListener('load', () =>{
     // canvas resizie
     resize();
   
-    ctx.strokeStyle = "blue";
-    ctx.strokeRect(50,50,200,200);
-    ctx.strokeStyle = "red";
-    ctx.strokeRect(100,100,200,200);
+    // ctx.strokeStyle = "blue";
+    // ctx.strokeRect(50,50,200,200);
+    // ctx.strokeStyle = "red";
+    // ctx.strokeRect(100,100,200,200);
+
+    ctx.beginPath();
+    ctx.moveTo(100,100);
+    ctx.lineTo(500,100);
+    ctx.lineTo(500,300);
+    ctx.closePath();
+    ctx.stroke();
+
 });
 window.addEventListener('resize', resize)
 
