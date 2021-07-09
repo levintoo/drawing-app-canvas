90a448e965f15641e79ae9654c8ccee518402e40
levintoo <tooklevin@gmail.com>
2021-07-09 22:25:41 +0300

    limitless drwaing

--- canvas.js
+++ canvas.js
@@ -11,15 +11,40 @@ window.addEventListener('load', () =>{
     // ctx.strokeStyle = "red";
     // ctx.strokeRect(100,100,200,200);
 
-    ctx.beginPath();
-    ctx.moveTo(100,100);
-    ctx.lineTo(500,100);
-    ctx.lineTo(500,300);
-    ctx.closePath();
+    // ctx.beginPath();
+    // ctx.moveTo(100,100);
+    // ctx.lineTo(500,100);
+    // ctx.lineTo(500,300);
+    // ctx.closePath();
+    // ctx.stroke();
+
+
+    let painting = "false";
+
+    function startPosition(){
+        painting= "true";
+    }
+    function finishedPosition(){
+        painting= "false";
+    }
+
+    canvas.addEventListener('mousedown', startPosition);
+    canvas.addEventListener('mouseup', finishedPosition);
+    canvas.addEventListener('mousemove', draw);
+
+function draw(e){
+    if (!painting) return;
+    ctx.lineWidth = 10;
+    ctx.lineCap = 'round';
+
+    ctx.lineTo(e.clientX, e.clientY);
     ctx.stroke();
+}
 
 });
-window.addEventListener('resize', resize)
+
+
+// window.addEventListener('resize', resize)
 
 function resize(){
     canvas.height = window.innerHeight;
